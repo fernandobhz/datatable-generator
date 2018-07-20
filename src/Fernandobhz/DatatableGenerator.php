dc7482d6d2c1c983c72422a11129288ee6015955
Fernando Reis Guimaraes <fernandobhz@gmail.com>
2018-07-20 16:37:53 -0300

    working tested

--- src/Fernandobhz/DatatableGenerator.php
+++ src/Fernandobhz/DatatableGenerator.php
@@ -3,61 +3,51 @@ namespace Fernandobhz;
 
 Class DatatableGenerator {
 	
-	public static function dtable($table, $title) {
-		self::code($table, $title, 'data-table');
+	public static function dtable($table, $cols) {
+		self::code($table, $cols, 'data-table');
 	}
 	
-	public static function dlist($table, $title) {
-		self::code($table, $title, 'data-table-list');
+	public static function dlist($table, $cols) {
+		self::code($table, $cols, 'data-table-list');
 	}
 	
-	public static function dnew($table, $title) {
-		self::code($table, $title, 'data-table-new');
+	public static function dnew($table, $cols) {
+		self::code($table, $cols, 'data-table-new');
 	}
 	
-	private static function code($table, $title, $cls) {
-		$table = json_encode(json_decode($table)); 
-		$cols = array_keys($rows[0]);
-		$rows = [];
-		foreach ( $table as $x ) $rows[] = array_values($x);
-		:?>
-			<div>
-				<div class=row>
-					<div class=row style="text-transform: uppercase; text-align: center">
-						<?=$title?>
-					</div>
-				</div>
-				<div class=row>
-					<div class=col>
-						<table 
-							id="mytable" 
-							class="table table-striped table-bordered $cls"
-							data-name="Pessoas" 
-							data-action="add">				
-							<thead>
-								<tr>
-									<?php foreach ( $cols as $col ): ?>
-									<th>
-										<?=$col?>
-									</th>
-									<?php endforeach; ?>
-								</tr>
-							</thead>
-							<tbody>
-								<?php foreach ( $rows as $row ): ?>
-								<tr>
-									<?php foreach ( $row as $val): ?>
-									<td>
-										<?=$val?>
-									</td>
-									<?php endforeach; ?>
-								</tr>
-								<?php endforeach; ?>
-							</tbody>
-						</table>
-					</div>
-				</div>
-			</div>
+	private static function code($table, $cols, $cls) {
+		$table = (array)json_decode(json_encode($table));
+		foreach ( $table as $key => $value ) $table[$key] = (array)$value;
+		
+		$rows = []; foreach ( $table as $x ) $rows[] = array_values($x);
+		//var_dump($cols); die();
+		?>
+			<table 
+				id="mytable" 
+				class="table table-striped table-bordered <?=$cls?>"
+				data-name="Pessoas" 
+				data-action="add">				
+				<thead>
+					<tr>
+						<?php foreach ( $cols as $col ): ?>
+						<th>
+							<?=$col?>
+						</th>
+						<?php endforeach; ?>
+					</tr>
+				</thead>
+				<tbody>
+					<?php foreach ( $rows as $row ): ?>
+					<tr>
+						<?php foreach ( $row as $val): ?>
+						<td>
+							<?=$val?>
+						</td>
+						<?php endforeach; ?>
+					</tr>
+					<?php endforeach; ?>
+				</tbody>
+			</table>
 		<?php		
 	}
 }
